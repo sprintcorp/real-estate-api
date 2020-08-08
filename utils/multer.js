6cbb83f74355cde13fdf30c0cea26d6e912d7f3f
sprintcorp <sprintcorp7@gmail.com>
2020-08-08 03:02:17 -0700

    Update file upload size

--- utils/multer.js
+++ utils/multer.js
@@ -23,7 +23,7 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
     storage: storage,
-    limits: { fileSize: 2024 * 2024 },
+    limits: { fileSize: 5 * 1024 * 1024 },
     fileFilter: fileFilter
 })
 
