21ac0d6632a2939d12eac90cae4b3dc70fea9f32
sprintcorp <sprintcorp7@gmail.com>
2020-08-08 16:33:41 -0700

    Update on user controller to get user house

--- controller/user.js
+++ controller/user.js
@@ -15,21 +15,10 @@ exports.getAgents = asyncHandler(async(req, res, next) => {
     });
 })
 
-exports.getAgent = asyncHandler(async(req, res, next) => {
-    const agent = await User.find({ _id: req.params.id })
+exports.getAgentHouse = asyncHandler(async(req, res, next) => {
+    const agent = await User.findById(req.params.user).populate('user');
     if (!agent) {
         return next(new ErrorResponse('No Agent Available with this id', 404))
-    }
-    res.status(200).json({
-        success: true,
-        data: agent
-    });
-})
-
-exports.getAgentHouse = asyncHandler(async(req, res, next) => {
-    const agent = await House.find({ user: req.params.user })
-    if (!agent) {
-        return next(new ErrorResponse('No Agent Available', 404))
     }
     res.status(200).json({
         success: true,
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const { getAgents, getAgentHouse, getAgent } = require('../controller/user');
+const { getAgents, getAgentHouse } = require('../controller/user');
 // const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', getAgents);
-router.get('/agent/:id', getAgent);
 router.get('/:user', getAgentHouse);
 
 
