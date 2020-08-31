34d0abcd68ff1b05d10f9a80eb094e3a8e115b30
sprintcorp <sprintcorp7@gmail.com>
2020-08-31 00:03:42 -0700

    Update on category model

--- model/Rating.js
+++ model/Rating.js
@@ -27,38 +27,38 @@ const RatingSchema = new mongoose.Schema({
 RatingSchema.index({ agent: 1, user: 1 }, { unique: true });
 
 // Static method to get avg rating and save
-RatingSchema.statics.getAverageRating = async function(agentId) {
-    const obj = await this.aggregate([{
-            $match: { agent: agentId }
-        },
-        {
-            $group: {
-                _id: agentId,
-                averageRating: { $avg: this.rating }
+// RatingSchema.statics.getAverageRating = async function(agentId) {
+//     const obj = await this.aggregate([{
+//             $match: { agent: agentId }
+//         },
+//         {
+//             $group: {
+//                 _id: agentId,
+//                 averageRating: { $avg: this.rating }
 
-            }
-        }
-    ]);
-    // console.log(obj[0].averageRating);
-    try {
-        await this.model('User').findByIdAndUpdate(agentId, {
-            averageRating: obj[0].averageRating
-        });
+//             }
+//         }
+//     ]);
+//     // console.log(obj[0].averageRating);
+//     try {
+//         await this.model('User').findByIdAndUpdate(agentId, {
+//             averageRating: obj[0].averageRating
+//         });
 
-    } catch (err) {
-        console.error(err);
+//     } catch (err) {
+//         console.error(err);
 
-    }
-};
+//     }
+// };
 
 // Call getAverageCost after save
-RatingSchema.post('save', async function() {
-    await this.constructor.getAverageRating(this.user);
-});
+// RatingSchema.post('save', async function() {
+//     await this.constructor.getAverageRating(this.user);
+// });
 
-// Call getAverageCost before remove
-RatingSchema.pre('remove', async function() {
-    await this.constructor.getAverageRating(this.user);
-});
+// // Call getAverageCost before remove
+// RatingSchema.pre('remove', async function() {
+//     await this.constructor.getAverageRating(this.user);
+// });
 
 module.exports = mongoose.model('Rating', RatingSchema);
